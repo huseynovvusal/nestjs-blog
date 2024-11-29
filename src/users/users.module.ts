bbe5f9aeacaf629357242acc10aa2ec20056dade
Vusal Huseynov <vslh333222111@gmail.com>
2024-11-29 23:08:27 +0400

    Testing the AccessTokenGuard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,13 +9,17 @@ import { UsersCreateManyProvider } from './providers/users-create-many.provider'
 import { CreateUserProvider } from './providers/create-user.provider';
 import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email.provider';
 import profileConfig from './config/profile.config';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    forwardRef(() => AuthModule),
     TypeOrmModule,
     TypeOrmModule.forFeature([User]),
     ConfigModule.forFeature(profileConfig),
+    forwardRef(() => AuthModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   controllers: [UsersController],
   providers: [
