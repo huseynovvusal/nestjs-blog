b2163fbd6975a1fa7fa155c0327fbc30073977a2
Vusal Huseynov <vslh333222111@gmail.com>
2024-12-21 19:51:19 +0400

    File Uploads

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,7 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { Express } from 'express';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Upload } from '../upload.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UploadToAwsProvider } from './upload-to-aws.provider';
+import { ConfigService } from '@nestjs/config';
+import { UploadFile } from '../interfaces/upload-file.interface';
+import { FileTypes } from '../enums/file-types.enum';
 
 @Injectable()
 export class UploadsService {
-  public async uploadFile(file: Express.Multer.File) {}
+  constructor(
+    @InjectRepository(Upload)
+    private readonly uploadsRepository: Repository<Upload>,
+    private readonly uploadToAwsProvider: UploadToAwsProvider,
+    private readonly configService: ConfigService,
+  ) {}
+
+  public async uploadFile(file: Express.Multer.File) {
+    if (
+      !['image/gif', 'image/jpeg', 'image/jpg', 'image/png'].includes(
+        file.mimetype,
+      )
+    ) {
+      throw new Error('Invalid file type');
+    }
+
+    try {
+      const name = await this.uploadToAwsProvider.fileUpload(file);
+
+      const uploadFile: UploadFile = {
+        name,
+        path: `${this.configService.get('appConfig.awsCloudFrontUrl')}/${name}`,
+        type: FileTypes.IMAGE,
+        mime: file.mimetype,
+        size: file.size,
+      };
+
+      const upload = this.uploadsRepository.create(uploadFile);
+
+      return await this.uploadsRepository.save(upload);
+    } catch (error) {
+      throw new ConflictException(error);
+    }
+  }
 }
